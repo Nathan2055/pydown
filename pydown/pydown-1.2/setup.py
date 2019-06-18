c9692705ee52ce3e4c808c61c814c41a427d5fca
Nathan Larsen <nlarsen@nathan2055.com>
2019-06-18 01:42:27 -0400

    1.2 release

---
+++ pydown/pydown-1.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pydown',
+    version='1.2',
+    author='Nathan2055',
+#   author_email='nathan2055@private',
+    packages=['pydown',],
+    url='http://pypi.python.org/pypi/pydown/',
+    license='BSD license',
+    description='A program that provides easy downloading to Python 3',
+    long_description=open('README.txt').read(),
+)
